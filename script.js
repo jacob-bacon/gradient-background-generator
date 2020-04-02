f754b8d23b5f42c6414e50143415e98d009e082c
Jacob Bacon <jake.bacn@gmail.com>
2020-04-02 05:33:31 -0500

    Add ability to change gradient direction

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var direction = document.querySelector(".direction");
 var bgTextOutput = document.querySelector("h5");
 var body = document.querySelector("body");
 var bgStyleText = "";
 
 function updateBg(){
-    bgStyleText = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-    bgTextOutput.innerText = "background: " + bgStyleText + ";";
+    bgStyleText = `linear-gradient(${direction.value}deg, ${color1.value}, ${color2.value})`;
+    bgTextOutput.innerText = `background: ${bgStyleText};`;
     body.style.background = bgStyleText;
 };
 
