7de2c3e4ba2df638a626cc8c03d8533758feadee
Jacob Bacon <jake.bacn@gmail.com>
2020-04-03 19:12:09 -0500

    add canvas and beginnings for custom color wheel

--- script.js
+++ script.js
@@ -5,6 +5,32 @@ var bgTextOutput = document.querySelector("h5");
 var body = document.querySelector("body");
 var bgStyleText = "";
 
+const canvas = document.getElementById('colorWheel');
+const ctx = canvas.getContext('2d');
+
+function drawColorWheel() {
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+    const wheelOuterRadius = centerX - 5;
+    const wheelInnerRadius = centerX - 35;
+    const arcStartDeg = 0;
+    const arcEndDeg = 2;
+    
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1;
+    
+    //outer wheel border
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, wheelOuterRadius, arcStartDeg, arcEndDeg * Math.PI);
+    ctx.stroke();
+
+    //inner wheel border
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, wheelInnerRadius, arcStartDeg, arcEndDeg * Math.PI);
+    ctx.stroke();
+}
+
+
 function updateBg(){
     bgStyleText = `linear-gradient(${direction.value}deg, ${color1.value}, ${color2.value})`;
     bgTextOutput.innerText = `background: ${bgStyleText};`;
@@ -12,10 +38,12 @@ function updateBg(){
 };
 
 updateBg();
+drawColorWheel();
 
 color1.addEventListener("input", updateBg);
 color2.addEventListener("input", updateBg);
 direction.addEventListener("input", updateBg);
+
 
 
 
