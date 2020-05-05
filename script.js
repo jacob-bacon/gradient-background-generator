a42dde93a59c98634c65e79b205b76697ad7302f
Jacob Bacon <jake.bacn@gmail.com>
2020-05-05 06:12:34 -0500

    got rid of global vars, finished converting color wheel and moveable color picker to canvas

--- script.js
+++ script.js
@@ -1,83 +1,85 @@
-let color1 = document.querySelector(".color1");
-let color2 = document.querySelector(".color2");
-let direction = document.querySelector(".direction");
-let bgTextOutput = document.querySelector("h5");
-let body = document.getElementById("gradientContainer");
-let bgStyleText = "";
-
-const bar = document.getElementById("colorBar");
-const barPicker = document.getElementById("barPicker");
-
-function movePicker(e, wheelCenterX, wheelCenterY, radius, halfPickerWidth) {
-    //use pythagorean theorem to find active picker"s distance from wheel center
-    let a = 0;
-    let b = 0;
-
-    //while wheel center is treated as (0,0), math operation changes based on whether picker
-    //position is +/- x or +/- y
-    if ((e.clientX >= wheelCenterX)) { 
-        a = Math.pow((e.clientX - wheelCenterX), 2);
-    } else {
-        a = Math.pow((wheelCenterX - e.clientX), 2);
-    };
-    if ((e.clientY >= wheelCenterY)) { 
-        b = Math.pow((e.clientY - wheelCenterY), 2);
-    } else {
-        b = Math.pow((wheelCenterY - e.clientY), 2);
-    };
-
-    let c = Math.sqrt(a + b);
-
-    if (c <= radius) {
-        wheelPicker.style.left = (e.clientX - halfPickerWidth) + "px";
-        wheelPicker.style.top = (e.clientY - halfPickerWidth) + "px";
-    }
-};
 
 
-function drawColorWheel() {
+function load() {
+    let color1 = document.querySelector(".color1");
+    let color2 = document.querySelector(".color2");
+    let direction = document.querySelector(".direction");
+    let bgTextOutput = document.querySelector("h5");
+    let body = document.getElementById("gradientContainer");
+    let bgStyleText = "";
+    let isMoving = false;
+
     const colorWheelCanvas = document.getElementById("colorWheel");
     const ctx = colorWheelCanvas.getContext("2d");
     const colorWheelImage = document.getElementById("colorWheelSrc");
-    const radius = colorWheelCanvas.offsetWidth / 2;
-    const wheelCenterX = (colorWheelCanvas.getBoundingClientRect().left) + radius;
-    const wheelCenterY = (colorWheelCanvas.getBoundingClientRect().top) + radius;
-    const wheelPicker = document.getElementById("wheelPicker");
-    let halfPickerWidth = wheelPicker.offsetWidth / 2;
-    let isMoving = false;
-    
-    ctx.drawImage(colorWheelImage, 0, 0, 204, 204, 0, 0, 204, 204);
-    colorWheelImage.style.display = "none";
+    let canvasStartX = (colorWheelCanvas.getBoundingClientRect().left);
+    let canvasStartY = (colorWheelCanvas.getBoundingClientRect().top);
+    const pickerDiameter = 4;
+    const pickerRadius = pickerDiameter / 2;
+    let pickerPos = [0,0];
 
-    document.querySelectorAll(".moveable").forEach(item => {
-        item.addEventListener("mousedown", e => {
+    
+    function drawColorWheel(pickerX, pickerY) {
+        canvasStartX = (colorWheelCanvas.getBoundingClientRect().left);
+        canvasStartY = (colorWheelCanvas.getBoundingClientRect().top);        
+        
+        ctx.drawImage(colorWheelImage, 0, 0, 204, 204, 0, 0, 204, 204);
+        colorWheelImage.style.display = "none";
+    
+        ctx.beginPath();
+        ctx.arc(pickerX + pickerRadius, pickerY + pickerRadius, pickerDiameter, 0, 2 * Math.PI);
+        ctx.lineWidth = 2;
+        ctx.stroke();
+        
+        console.log(Math.round(pickerX), Math.round(pickerY));
+        pickerPos = [pickerX, pickerY];
+        return pickerPos;
+    };
+    
+
+    function updateBg(){
+        bgStyleText = `linear-gradient(${direction.value}deg, ${color1.value}, ${color2.value})`;
+        bgTextOutput.innerText = `background: ${bgStyleText};`;
+        body.style.background = bgStyleText;
+    };
+
+
+    //add event listeners
+    colorWheelCanvas.addEventListener("mousedown", e => {
+        e.preventDefault();
+        isMoving = true;
+        drawColorWheel(e.clientX - canvasStartX, e.clientY - canvasStartY);
+    });
+
+    colorWheelCanvas.addEventListener("mousemove", e => {
+        if (isMoving === true) {
             e.preventDefault();
-            isMoving = true;
-            movePicker(e, wheelCenterX, wheelCenterY, radius, halfPickerWidth);
-        });
+            drawColorWheel(e.clientX - canvasStartX, e.clientY - canvasStartY);
+        };
     });
 
     window.addEventListener("mouseup", e => {
         isMoving = false;
     });
 
-    colorWheelCanvas.addEventListener("mousemove", e => {
-        if (isMoving === true) {
-            e.preventDefault();
-            movePicker(e, wheelCenterX, wheelCenterY, radius, halfPickerWidth);
-        };
+    window.addEventListener("resize", e => {
+        drawColorWheel(pickerPos[0], pickerPos[1]);
     });
-}
 
-function updateBg(){
-    bgStyleText = `linear-gradient(${direction.value}deg, ${color1.value}, ${color2.value})`;
-    bgTextOutput.innerText = `background: ${bgStyleText};`;
-    body.style.background = bgStyleText;
+    color1.addEventListener("input", updateBg);
+    color2.addEventListener("input", updateBg);
+    direction.addEventListener("input", updateBg);
+
+    
+    function randomIntBetween(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+
+    pickerPos = drawColorWheel(randomIntBetween(30, 170), randomIntBetween(30, 170));
+    updateBg();
 };
 
-updateBg();
-drawColorWheel();
+load();
 
-color1.addEventListener("input", updateBg);
-color2.addEventListener("input", updateBg);
-direction.addEventListener("input", updateBg);
+
+
